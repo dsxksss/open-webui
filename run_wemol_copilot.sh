8eab5ca2573ce2bf06cac943b2e1beec6267e21f
VentCat <2546650292@qq.com>
2025-03-21 10:47:35 +0800

    版本更新

--- run_wemol_copilot.sh
+++ run_wemol_copilot.sh
@@ -1,7 +1,7 @@
-docker run -d --name wemol_copilot-1.1 -p 8040:8080 --gpus all \
+docker run -d --name wemol_copilot-1.2 -p 8040:8080 --gpus all \
 -v /data/PRG/WeMolCopilot/:/app/backend/data \
 -e OPENAI_API_BASE_URL='https://api.zhizengzeng.com/v1' \
 -e OPENAI_API_KEY='sk-zk24ca892d6bba0d536c60457590a62889366ac4bfd88471' \
 -e DEFAULT_USER_ROLE=user \
 -e WEBUI_NAME='WeMol Copilot' \
-wemol_copilot:1.1
+wemol_copilot:1.2
